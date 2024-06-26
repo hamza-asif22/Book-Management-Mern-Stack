bea0919bfd4743c03b59a3669d7779942683d5eb
HAMZA ASIF <hamzaasifbaig190@gmail.com>
2024-06-26 11:14:34 +0500

    Day-2

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,31 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
-import { Book } from './models/bookmodel.js';
+import booksRoute from './routes/booksRoute.js'; // Adjust the path as necessary
+import cors from 'cors';
+
 
 const app = express();
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+//app.use(cors());
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(200).send('Welcome to Mern Stack'); 
 });
 
-app.post('/books', async (request, response) => {
-    try {
-        const { title, author, publishYear } = request.body;
-        
-        if (!title || !author || !publishYear) {
-            return response.status(400).send({
-                message: 'Please fill all the fields',
-            });
-        }
-
-        const newBook = {
-            title,
-            author,
-            publishYear,
-        };
-
-        const book = await Book.create(newBook);
-        return response.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-app.get('/books', async (request, response) => {
-    try {
-        const books = await Book.find({});
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        }
-        );
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
 .connect(mongoDBURL)
