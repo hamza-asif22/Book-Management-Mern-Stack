0d7252c32a2fdd5bdb487a580c6a9d22dc68ea2c
hamza-asif22 <hamzaasifbaig190@gmail.com>
2025-04-08 20:30:44 +0500

    Changes

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb://localhost:27017/';
+export const mongoDBURL = 'mongodb+srv://hamzaab2209:QYs0Oq8efwCRZgvv@bookmanagement.m4d5crr.mongodb.net/?retryWrites=true&w=majority&appName=bookmanagement';
